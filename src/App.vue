cf8091de7a8eaf1b0cdef22ee6ad005e9f4d3a3c
Sherman Rose <sherman@shermanrose.uk>
2020-03-01 13:07:54 +0000

    Created Visualiser component with canvas entry point code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Visualiser />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Visualiser from "./components/Visualiser"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Visualiser
   }
 }
 </script>
---
+++ src/components/Visualiser.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div id="view_container">
+            <div id="canvas_block">
+                <canvas id="c" tabindex="0" resize></canvas>
+            </div>
+            <div id="renderContainer"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import BareViewManager from "../../3DuF/src/app/view/bareViewManager"
+import Registry from "../../3DuF/src/app/core/registry"
+import Examples from "../../3DuF/src/app/examples/jsonExamples"
+
+    export default {
+       mounted() {
+        let viewManager = new BareViewManager();
+
+        Registry.viewManager = viewManager;
+
+        viewManager.loadDeviceFromJSON(JSON.parse(Examples.example2));
+        viewManager.updateGrid();
+        Registry.currentDevice.updateView();
+
+        window.dev = Registry.currentDevice;
+        window.Registry = Registry;
+
+        window.view = Registry.viewManager.view;
+       }
+    }
+</script>
+
+<style lang="scss" scoped>
+#view-container {
+    width: 100%;
+    height: 100%;
+}
+</style>
